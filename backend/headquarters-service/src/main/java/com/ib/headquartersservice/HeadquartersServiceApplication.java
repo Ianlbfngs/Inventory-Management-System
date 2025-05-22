043e4ff682ebd5f940fb79a6fd7392eb434289f0
Ian Badino <ianlucab@hotmail.com.ar>
2025-05-22 03:13:01 -0300

    initial settings

--- backend/headquarters-service/src/main/java/com/ib/headquartersservice/HeadquartersServiceApplication.java
+++ backend/headquarters-service/src/main/java/com/ib/headquartersservice/HeadquartersServiceApplication.java
@@ -2,8 +2,10 @@ package com.ib.headquartersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HeadquartersServiceApplication {
 
     public static void main(String[] args) {
